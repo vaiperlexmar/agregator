1d8689d97cc8baa993505b8716f9acd2174e8093
vaiperlexmar <vaiperlexmar@gmail.com>
2025-02-28 21:40:55 +0300

    Add request for rss. Start making parsing logic

--- src/public/index.php
+++ src/public/index.php
@@ -1,6 +1,48 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 require __DIR__ . '/../vendor/autoload.php';
 
+$ch = curl_init();
+$url = "https://pravoslavie.ru/xml/full.xml";
+
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+
+$response = curl_exec($ch);
+
+curl_close($ch);
+
+$xml = simplexml_load_string($response);
+
+$html = '
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>Дока</title>
+  </head>
+  <body>
+';
+
+function xmlToHtml($xml) {
+    $html = '';
+    foreach ($xml->children() as $child) {
+        if ($child->children()->count() > 0) {
+            $html .= '<div class="' . $child->getName() . '">';
+            $html .= xmlToHtml($child);
+            $html .= '</div>';
+        } else {
+            $html .= '<div class="' . $child->getName() . '">';
+        }
+    }
+
+    return $html;
+}
+
+$html .= xmlToHtml($xml);
+echo $html;
